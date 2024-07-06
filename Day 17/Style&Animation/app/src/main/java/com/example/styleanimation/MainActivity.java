7f806a5b2deb3d256cadb8b9ddaac6c3320a28d0
Vighnesh Salunkhe <vighneshsalunkhe949@gmail.com>
2024-07-06 14:09:24 +0530

    new project added

--- Day 17/Style&Animation/app/src/main/java/com/example/styleanimation/MainActivity.java
+++ Day 17/Style&Animation/app/src/main/java/com/example/styleanimation/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
             }
         });
     }
--- Day 17/Style&Animation/app/src/main/java/com/example/styleanimation/SecondActivity.java
+++ Day 17/Style&Animation/app/src/main/java/com/example/styleanimation/SecondActivity.java
@@ -1,11 +1,15 @@
 package com.example.styleanimation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity  extends AppCompatActivity {
+    Button customButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,15 @@ public class SecondActivity  extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
 
+
+        customButton = findViewById(R.id.customButton);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
